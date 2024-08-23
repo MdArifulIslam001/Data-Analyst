4a1ca604797e46c95e8919c8fb8956c228e291d9
Ariful Islam <md._ariful.islam@smail.th-koeln.de>
2024-08-23 12:36:15 +0200

    SQL Advance topics

---
+++ MySQL/15. CTEs.sql
@@ -0,0 +1,43 @@
+-- CTEs -- Comman Table Expression
+
+WITH CTE_Example as
+(
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+FROM employee_demographics as dem
+JOIN employee_salary as sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT AVG(avg_sal)
+FROM CTE_Example
+;
+
+WITH CTE_Example as
+(
+SELECT employee_id, gender, birth_date
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+),
+CTE_Example2 as
+(
+SELECT employee_id, salary
+FROM employee_salary
+WHERE salary > 50000
+)
+SELECT *
+FROM CTE_Example
+JOIN CTE_Example2
+	ON CTE_Example.employee_id = CTE_Example2.employee_id
+;
+
+WITH CTE_Example (Gender, AVG_Sal, MAX_Sal, MIN_Sal, COUNT_Sal) as
+(
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+FROM employee_demographics as dem
+JOIN employee_salary as sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT *
+FROM CTE_Example
+;
